fabc168bfdb7ac8c10e1a311d56842637594e1b3
2riing <5032303000@naver.com>
2022-10-20 19:05:52 +0900

    feat: widget 생성, 스타일링

--- sti/component/studyroom/TodoList.tsx
+++ sti/component/studyroom/TodoList.tsx
@@ -2,6 +2,9 @@
 
 import React from 'react';
 import Draggable from 'react-draggable';
+import todo from '../../styles/TodoList.module.css';
+import widget from '../../styles/Widget.module.css';
+import styles from '../../styles/Home.module.css';
 
 interface Test {}
 
@@ -9,7 +12,28 @@ const TodoList: Test = () => {
   return (
     <>
       <Draggable>
-        <div>Todo</div>
+        <div className={(widget.widget, todo.todoWidget)}>
+          <div className={(widget.widgetHeader, todo.todoHeader)}>
+            Todo
+            <span className={widget.CloseWidgetBtn}>
+              <div className={(styles.stiButton, styles.light)}>
+                <button type="button">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="2"
+                    fill="none"
+                    class="minus-icon light"
+                  >
+                    <path d="M0 0h12v2H0V0z"></path>
+                  </svg>
+                </button>
+              </div>
+            </span>
+          </div>
+          <div className={widget.widgetContent}>content</div>
+          <div className={widget.widgetFooter}>footer</div>
+        </div>
       </Draggable>
     </>
   );
