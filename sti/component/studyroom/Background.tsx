e182a749882afb459cf3be1329f399158a464725
2riing <5032303000@naver.com>
2022-10-23 18:05:33 +0900

    feat: 배경 유투브 전체 화면

--- sti/component/studyroom/Background.tsx
+++ sti/component/studyroom/Background.tsx
@@ -16,7 +16,10 @@ const Background: Test = () => {
         <PlayerContainer>
           <PlayerContainer2>
             <iframe
-              src="https://www.youtube.com/embed/kVIKJb0JJz8?autoplay=1&mute=0&controls=0&loop=1&modestbranding=1&disablekb=1&playsinline=1&showinfo=0&iv_load_policy=3&enablejsapi=1"
+              src="https://www.youtube.com/embed/kVIKJb0JJz8?
+              autoplay=1&mute=0&controls=0&loop=1&modestbranding=1&
+              disablekb=1&playsinline=1&showinfo=0&iv_load_policy=3&enablejsapi=1s
+              &allowfullscreen=1&frameborder=0"
               height="100%"
               width="100%"
               allow="autoplay"
@@ -30,20 +33,33 @@ const Background: Test = () => {
 
 const VideoPlayer = styled.div`
   position: fixed;
+  z-index: -1;
+  top: -12%;
+  left: 0;
   overflow: hidden;
-  z-index: 0;
   user-select: text;
+  width: 100vw;
+  height: 100vh;
 `;
 
 const PlayerContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  max-aspect-ratio: 16 / 9;
 `;
 
 const PlayerContainer2 = styled.div`
-  width: 100%;
-  height: 100%;
+  position: fixed;
+  pointer-events: none;
+  top: 50%;
+  left: 50%;
+  width: 100vw;
+  height: 100vh;
+  transform: translate(-50%, -50%);
+  @media (max-aspect-ratio: 16 / 9) {
+    width: 177.78vh !important;
+    height: 120% !important;
+  }
 `;
 
 export default Background;
--- sti/pages/studyroom.tsx
+++ sti/pages/studyroom.tsx
@@ -4,16 +4,16 @@ import React from 'react';
 import Background from '../component/studyroom/Background';
 import Timer from '../component/studyroom/Timer';
 import TodoList from '../component/studyroom/TodoList';
+import styled from 'styled-components';
 
 interface Test {}
 
 const StudyRoom: Test = () => {
   return (
     <div>
-      <Background />
-      <TodoList />
-      <Timer />
       StudyRoom
+      <TodoList />
+      <Background />
     </div>
   );
 };
