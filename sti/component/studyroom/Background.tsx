c90641d1477f452f7e6d6a4ec9474dedf68495f6
2riing <5032303000@naver.com>
2022-10-23 17:20:22 +0900

    feat: 유투브 플레이어 background에 추가

--- sti/component/studyroom/Background.tsx
+++ sti/component/studyroom/Background.tsx
@@ -1,1 +1,49 @@
-export {};
+// @ts-nocheck
+
+import React from 'react';
+import styled from 'styled-components';
+import Draggable from 'react-draggable';
+import bac from '../../styles/TodoList.module.css';
+import widget from '../../styles/Widget.module.css';
+import styles from '../../styles/Home.module.css';
+
+interface Test {}
+
+const Background: Test = () => {
+  return (
+    <>
+      <VideoPlayer>
+        <PlayerContainer>
+          <PlayerContainer2>
+            <iframe
+              src="https://www.youtube.com/embed/kVIKJb0JJz8?autoplay=1&mute=0&controls=0&loop=1&modestbranding=1&disablekb=1&playsinline=1&showinfo=0&iv_load_policy=3&enablejsapi=1"
+              height="100%"
+              width="100%"
+              allow="autoplay"
+            ></iframe>
+          </PlayerContainer2>
+        </PlayerContainer>
+      </VideoPlayer>
+    </>
+  );
+};
+
+const VideoPlayer = styled.div`
+  position: fixed;
+  overflow: hidden;
+  z-index: 0;
+  user-select: text;
+`;
+
+const PlayerContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+const PlayerContainer2 = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export default Background;
--- sti/pages/studyroom.tsx
+++ sti/pages/studyroom.tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 
 import React from 'react';
+import Background from '../component/studyroom/Background';
 import Timer from '../component/studyroom/Timer';
 import TodoList from '../component/studyroom/TodoList';
 
@@ -9,6 +10,7 @@ interface Test {}
 const StudyRoom: Test = () => {
   return (
     <div>
+      <Background />
       <TodoList />
       <Timer />
       StudyRoom
