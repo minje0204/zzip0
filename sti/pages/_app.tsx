b0203516ef5ad8e311a30609c1eac2c1a98d32d1
정지은 <lisajieun@naver.com>
2022-10-24 17:26:18 +0900

    chore: 팔레트 색상 설정(primary, secondary, navbar)

--- sti/pages/_app.tsx
+++ sti/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// @ts-nocheck
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
