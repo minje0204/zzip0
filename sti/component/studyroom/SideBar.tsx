9cd6f3fc84cc5f079cf581dc2445a74f25f24d6c
soohong <5032303000@naver.com>
2022-10-25 13:54:24 +0900

    feat: volume add

--- sti/component/studyroom/SideBar.tsx
+++ sti/component/studyroom/SideBar.tsx
@@ -3,24 +3,21 @@
 
 import * as React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
+
+import Volume from './Volume'
+
+// mui
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+
 
 const drawerWidth = 300;
 
@@ -81,6 +78,11 @@ export default function SideBar() {
     setOpen(false);
   };
 
+  const SideBarUnderContainer = styled("div")(({ theme }) => ({
+    padding: theme.spacing(3),
+
+  }));
+
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
@@ -117,7 +119,9 @@ export default function SideBar() {
           </IconButton>
         </DrawerHeader>
         <Divider />
-
+        <SideBarUnderContainer>
+          <Volume />
+        </SideBarUnderContainer>
       </Drawer>
       <Main open={open}>
         <DrawerHeader />
@@ -125,3 +129,5 @@ export default function SideBar() {
     </Box>
   );
 }
+
+
