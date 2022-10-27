0d3a4fa77fb9eec3021ce9a09abe4a1495b3855f
sally <sally2596@gmail.com>
2022-10-27 17:56:13 +0900

    chore: 방 히스토리 엔티티 생성

---
+++ backend/src/main/java/com/a401/backend/domain/Room/domain/RoomHistory.java
@@ -0,0 +1,31 @@
+package com.a401.backend.domain.Room.domain;
+
+import com.a401.backend.domain.member.domain.Member;
+import com.a401.backend.domain.model.RoomAction;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RoomHistory {
+    @Id
+    @GeneratedValue
+    private long roomHistoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ROOM_ID")
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    private RoomAction roomAction;
+    private LocalDateTime actionTime;
+
+}
