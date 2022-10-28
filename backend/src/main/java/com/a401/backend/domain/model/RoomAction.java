141c03f4e049a8049fa2652219cb5d62d8e21c4d
sally <sally2596@gmail.com>
2022-10-28 15:34:35 +0900

    chore: RoomAction 추가

--- backend/src/main/java/com/a401/backend/domain/model/RoomAction.java
+++ backend/src/main/java/com/a401/backend/domain/model/RoomAction.java
@@ -1,5 +1,17 @@
 package com.a401.backend.domain.model;
 
 public enum RoomAction {
-    ENTER,EXIT,KICKOUT,OPEN,
+
+    // 방 개설, 입장, 퇴장, 강퇴
+    OPEN,
+    ENTER,
+    EXIT,
+    KICKOUT,
+
+    // OpenVidu 관련
+    SESSION_CREATE,
+    SESSION_EXIT,
+    SESSION_ENTER,
+    CAMERA_ON,
+    CAMERA_OFF,
 }
