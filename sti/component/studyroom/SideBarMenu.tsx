0647804ec12127f85f40b264497e2d02353c3051
soohong <5032303000@naver.com>
2022-10-28 10:55:33 +0900

    chore: todo, timer btn 아이콘 추가

--- sti/component/studyroom/SideBarMenu.tsx
+++ sti/component/studyroom/SideBarMenu.tsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 
 interface Test { }
@@ -11,11 +13,45 @@ const SideBarMenu: Test = () => {
 
   return (
     <SideBarMenuContainer>
-      <div>
-        <Button variant="outlined">Todo</Button>
-        <Button variant="outlined">Timer</Button>
-        <Button variant="outlined">Clock</Button>
-      </div>
+      <IconButton
+      variant="outlined"
+      style={{ display: "block", textAlign: "center" }}
+      sx={{
+        width: 70,
+        height: 70,
+        backgroundColor: 'white',
+        border: 1,
+        borderColor: '#e9e9e9',
+        padding: 1.7,
+        borderRadius: 2,
+        margin: 0.3,
+
+      }}
+      size="medium"
+    >
+      <img src={`/todo.png`} style={{ width: '23px' }} />
+      <Typography variant="caption" display="block" >TODO</Typography>
+    </IconButton>
+    <IconButton
+    variant="outlined"
+    style={{ display: "block", textAlign: "center" }}
+    sx={{
+      width: 70,
+      height: 70,
+      backgroundColor: 'white',
+      border: 1,
+      borderColor: '#e9e9e9',
+      padding: 1.7,
+      borderRadius: 2,
+      margin: 0.3,
+
+    }}
+    size="medium"
+  >
+    <img src={`/stopwatch.png`} style={{ width: '23px' }} />
+    <Typography variant="caption" display="block" >TIMER</Typography>
+  </IconButton>
+
     </SideBarMenuContainer>
   );
 };
