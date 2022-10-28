409066808df7789a3717783f7b867ee2df95d529
soohong <5032303000@naver.com>
2022-10-28 15:34:59 +0900

    feat: api base url 생성, axios설치

---
+++ sti/api.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://localhost:5000/api';
+
+const API = {
+  GETROOM: `${BASE_URL}/room/list`,
+  CREATEROOM: `${BASE_URL}/room`,
+  EXITROOM: `${BASE_URL}/room/exit`,
+}
+
+export default API;
