8a08fe66418999584faed856cee12c504a32aee5
2riing <5032303000@naver.com>
2022-10-30 18:40:34 +0900

    feature/FE/NavBarLink

--- sti/component/Navbar.tsx
+++ sti/component/Navbar.tsx
@@ -2,36 +2,49 @@
 
 import * as React from 'react';
 import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import Link from 'next/link';
 
 function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="navbar">
         <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            ZZip_0
-          </Typography>
+          <Link href={`/`}>
+            <Button variant="text" color="inherit" sx={{ mr: 1 }} size="large">
+              <b>ZZip_0</b>
+            </Button>
+          </Link>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <Button variant="text" color="inherit" sx={{ mr: 1 }}>
-              Join Study
-            </Button>
-            <Button variant="text" color="inherit" sx={{ mr: 1 }}>
-              Create Study
-            </Button>
-            <Button variant="text" color="inherit" disabled sx={{ mr: 1 }}>
-              Pricing
-            </Button>
-            <Button variant="text" color="inherit" sx={{ mr: 1 }}>
-              Report
-            </Button>
-            <Button
-              variant="contained"
-              color="darkButton"
-              sx={{ color: 'primary.main' }}
-            >
-              Sign In
-            </Button>
+            <Link href={`/`}>
+              <Button variant="text" color="inherit" sx={{ mr: 1 }}>
+                Home
+              </Button>
+            </Link>
+            <Link href={`/roomlist`}>
+              <Button variant="text" color="inherit" sx={{ mr: 1 }}>
+                Join Study
+              </Button>
+            </Link>
+            <Link href={`/payment`}>
+              <Button variant="text" color="inherit" disabled sx={{ mr: 1 }}>
+                Pricing
+              </Button>
+            </Link>
+            <Link href={`/report`}>
+              <Button variant="text" color="inherit" sx={{ mr: 1 }}>
+                Report
+              </Button>
+            </Link>
+            <Link href={`/signin`}>
+              <Button
+                variant="contained"
+                color="darkButton"
+                sx={{ color: 'primary.main' }}
+              >
+                Sign In
+              </Button>
+            </Link>
           </Box>
         </Toolbar>
       </AppBar>
