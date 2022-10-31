299f63e1b594b05134a249b0e919cc8e82f7426c
soohong <5032303000@naver.com>
2022-10-31 16:00:03 +0900

    feat: todo api

--- sti/lib/api/room.js
+++ sti/lib/api/room.js
@@ -1,64 +1,68 @@
-
-
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_KEY,
-})
+  baseURL: process.env.NEXT_PUBLIC_API_KEY
+});
 
 async function roomGetAPI(data) {
   try {
-    const res = await api.get(`room/list?page=${data}`)
-    return res
+    const res = await api.get(`room/list?page=${data}`);
+    return res;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
 async function roomPostAPI(header, data) {
   try {
-    const res = await api.post(`room`, { data: data, headers: headers })
-    return res
+    const res = await api.post(`room`, { data: data, headers: headers });
+    return res;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
 async function roomCloseAPI(header, data) {
   try {
-    const res = await api.patch(`room`, { data: data, headers: headers })
-    return res
+    const res = await api.patch(`room`, { data: data, headers: headers });
+    return res;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
 async function roomExitAPI(header, data) {
   try {
-    const res = await api.patch(`room/exit`, { data: data, headers: headers })
-    return res
+    const res = await api.patch(`room/exit`, { data: data, headers: headers });
+    return res;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
 async function roomSearchAPI(header, data) {
   try {
-    const res = await api.get(`room/search?q=${data}`, { headers: headers })
-    return res
+    const res = await api.get(`room/search?q=${data}`, { headers: headers });
+    return res;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
-  async function roomGrantAPI(header, data) {
-    try {
-      const res = await api.get(`room/grant`, { data: data, headers: headers })
-      return res
-    } catch (err) {
-      console.log(err)
-    }
+async function roomGrantAPI(header, data) {
+  try {
+    const res = await api.get(`room/grant`, { data: data, headers: headers });
+    return res;
+  } catch (err) {
+    console.log(err);
   }
+}
 
-export { roomGetAPI, roomPostAPI, roomCloseAPI, roomExitAPI, roomSearchAPI, roomGrantAPI }
-
+export {
+  roomGetAPI,
+  roomPostAPI,
+  roomCloseAPI,
+  roomExitAPI,
+  roomSearchAPI,
+  roomGrantAPI
+};
--- sti/lib/api/todo.js
+++ sti/lib/api/todo.js
@@ -1,1 +1,52 @@
-  export {}
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_KEY
+});
+
+async function todoGetAPI(data) {
+  try {
+    const res = await api.get(`todo/${date}`, { headers: headers });
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function todoPostAPI(header, data) {
+  try {
+    const res = await api.post(`todo/${data.date}`, {
+      data: data,
+      headers: headers
+    });
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function todoPatchAPI(header, data) {
+  try {
+    const res = await api.patch(`todo/${data.id}`, {
+      data: data,
+      headers: headers
+    });
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function todoDeleteAPI(header, data) {
+  try {
+    const res = await api.delete(`todo/${data.id}`, {
+      data: data,
+      headers: headers
+    });
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export { todoGetAPI, todoPostAPI, todoPatchAPI, todoDeleteAPI };
