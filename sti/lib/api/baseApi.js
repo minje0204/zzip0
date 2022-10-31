d0f3fafe05acbf5b72f268a1e5f2993cd1883e5e
soohong <5032303000@naver.com>
2022-10-31 14:59:30 +0900

    feat: api 모듈 초안

---
+++ sti/lib/api/baseApi.js
@@ -0,0 +1,45 @@
+
+export const API = {
+  ROOM: `${PUBLIC_API_KEY}/room`,
+  MEMBER: `${PUBLIC_API_KEY}/member`,
+};
+
+
+// const API22 = {
+//   // member
+//   LOGIN: `${BASE_URL}/member/signin`,
+//   SIGNUP: `${BASE_URL}/member/singup`,
+//   MYACCOUNT: `${BASE_URL}/user/me`,
+//   WITHDRAWAL: `${BASE_URL}/member/withdrawal`,
+//   UPDATEMEMBER: `${BASE_URL}/member`,
+
+//   // todo
+//   TODO: `${BASE_URL}/todo`,
+
+//   // background
+//   BACK: `${BASE_URL}/background`,
+//   BACKLIKE: `${BASE_URL}/background/like`,
+
+//   // room
+//   GETROOM: `${BASE_URL}/room/list`,
+//   ROOM: `${BASE_URL}/room`,
+//   EXITROOM: `${BASE_URL}/room/exit`,
+//   SEARCHROOM: `${BASE_URL}/room/search`,
+//   ROOMGRANT: `${BASE_URL}/room/exile`,
+
+//   // follow
+//   FOLLOWLIST: `${BASE_URL}/follow`,
+//   UNFOLLOW: `${BASE_URL}/unfollow`,
+//   FOLLOWING: `${BASE_URL}/following`,
+
+//   // memo
+//   MEMO: `${BASE_URL}/memo`,
+
+//   // timeview
+
+//   // timelog
+//   TIMELOG: `${BASE_URL}/timelog`
+// };
+
+export default API;
+
---
+++ sti/lib/api/room.js
@@ -0,0 +1,22 @@
+
+
+import axios from 'axios';
+
+const PUBLIC_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+const api = axios.create({
+  baseURL: PUBLIC_API_KEY,
+})
+
+async function roomGetAPI(data) {
+  try {
+    const res = await api.get(`room/list?page=${data}`, { headers })
+    return res
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+
+export { roomGetAPI }
+
