af4739875d69c0d8ea1cbf9872a16e78a6c36a68
sally <sally2596@gmail.com>
2022-10-31 11:26:29 +0900

    chore: todolistResponseDto 고민

--- backend/src/main/java/com/a401/backend/domain/TodoList/dto/response/TodoListResponseDto.java
+++ backend/src/main/java/com/a401/backend/domain/TodoList/dto/response/TodoListResponseDto.java
@@ -1,6 +1,9 @@
 package com.a401.backend.domain.TodoList.dto.response;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -11,12 +14,9 @@ public class TodoListResponseDto {
 
     private LocalDateTime date;
 
-    // 로그인한 유저의 정보
-    // 추후에 추가
-
-
+    // TODO: 2022-10-31 로그인한 유저의 정보를 response에 담아서 줄 필요가 있는가? 
     @Builder
-    public TodoListResponseDto(LocalDateTime date){
+    public TodoListResponseDto(LocalDateTime date) {
         this.date = date;
     }
 
