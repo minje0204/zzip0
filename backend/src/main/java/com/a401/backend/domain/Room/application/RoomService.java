192e9e39915fe8204400d4936d174d4fd94a6b6f
sally <sally2596@gmail.com>
2022-11-01 17:04:59 +0900

    feat: 방 입장 완료

--- backend/src/main/java/com/a401/backend/domain/Room/application/RoomService.java
+++ backend/src/main/java/com/a401/backend/domain/Room/application/RoomService.java
@@ -7,11 +7,10 @@ import com.a401.backend.domain.member.domain.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface RoomService {
     Page<RoomResponseDto> getAllActivateRooms(Pageable pageable);
 
-
     Room createRoom(RoomRequestDto roomRequestDto, Member member);
+
+    Room findRoom(Long roomId);
 }
--- backend/src/main/java/com/a401/backend/domain/Room/dao/RoomRepository.java
+++ backend/src/main/java/com/a401/backend/domain/Room/dao/RoomRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room,Long> {
+public interface RoomRepository extends JpaRepository<Room, Long> {
     Page<Room> findByActivateTrue(Pageable pageable);
+
+    Optional<Room> findByRoomId(Long roomId);
 //    Optional<Room> findByOwnerId(long ownerId);
 }
