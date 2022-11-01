603b8aa451f659546d942fbd1112f894e349ddae
soohong <5032303000@naver.com>
2022-11-01 10:57:57 +0900

    feat: datepicket add, defaultValue set today

--- sti/component/studyroom/Todo/TodoList.tsx
+++ sti/component/studyroom/Todo/TodoList.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // css
 import todo from '../../../styles/TodoList.module.css';
@@ -11,6 +11,7 @@ import styles from '../../../styles/Home.module.css';
 import Draggable from 'react-draggable';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
+import TextField from '@mui/material/TextField';
 
 // recoil
 import { todosState } from '../../../lib/recoil/todo';
@@ -24,7 +25,25 @@ interface Test {}
 
 const TodoList: Test = () => {
   const todos = useRecoilValue(todosState);
+  const [date, setDate] = useState('');
   const nodeRef = React.useRef(null);
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = ('0' + (today.getMonth() + 1)).slice(-2);
+  const day = ('0' + today.getDate()).slice(-2);
+  const dateStr = year + '-' + month + '-' + day;
+
+  useEffect(() =>{
+    console.log(dateStr)
+  }, [])
+
+
+  const changeDate = (e) => {
+    console.log(e.target.value);
+    setDate(e.target.value);
+  };
+
   return (
     <>
       <Draggable nodeRef={nodeRef} defaultPosition={{ x: 300, y: 300 }}>
@@ -43,12 +62,27 @@ const TodoList: Test = () => {
               </button>
             </div>
           </div>
+
+          {/* widget content */}
           <div className={(widget.widgetContent, todo.todoWidgetContent)}>
+            <div>
+              <TextField
+                id="date"
+                type="date"
+                defaultValue={dateStr}
+                sx={{ width: 220 }}
+                onChange={(e) => changeDate(e)}
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
+            </div>
             {todos.map((todo) => (
               <TodoItems key={todo.id} data={todo} />
             ))}
             <TodoInput />
           </div>
+
           <div className={widget.widgetFooter}>
             <Box sx={{ width: '80%' }}>
               <LinearProgress
