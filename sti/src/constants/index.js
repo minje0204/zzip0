5a9d06d02db7cffe990136bbd3beabb6428eb9e8
soohong <5032303000@naver.com>
2022-11-01 15:21:45 +0900

    fix: url

--- sti/src/constants/index.js
+++ sti/src/constants/index.js
@@ -4,4 +4,4 @@ export const ACCESS_TOKEN = 'accessToken';
 export const OAUTH2_REDIRECT_URI = `${API_BASE_URL}/oauth2/redirect`;
 
 export const GOOGLE_AUTH_URL =
-  API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
+  API_BASE_URL + '/api/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
