9d555389e4ec1c3780b13c7362dacc59ef974f58
soohong <5032303000@naver.com>
2022-11-01 16:36:11 +0900

    fix: redirect url api add

--- sti/src/constants/index.js
+++ sti/src/constants/index.js
@@ -1,7 +1,7 @@
 export const API_BASE_URL = 'https://zzip0.com'
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = `${API_BASE_URL}/oauth2/redirect`;
+export const OAUTH2_REDIRECT_URI = `${API_BASE_URL}/api/oauth2/redirect`;
 
 export const GOOGLE_AUTH_URL =
   API_BASE_URL + '/api/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
