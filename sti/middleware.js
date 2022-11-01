fe52519c668d066640ebb8d4b023f750966a3e1c
soohong <5032303000@naver.com>
2022-11-01 16:44:19 +0900

    fix: middleware update

--- sti/middleware.js
+++ sti/middleware.js
@@ -1,9 +1,10 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
+  console.log('hh')
   const url = request.nextUrl.clone();
-
   if (url.pathname === '/oauth2/redirect') {
+    console.log('middleware')
     url.pathname = '/';
     return NextResponse.redirect(url);
   }
