991216364c49e2d5034c3683a5e40e3cae549c5d
soohong <5032303000@naver.com>
2022-11-02 10:15:09 +0900

    feat: Add roomUrl in studyroom page

--- sti/pages/studyroom/[roomUrl].tsx
+++ sti/pages/studyroom/[roomUrl].tsx
@@ -1,14 +1,19 @@
 // @ts-nocheck
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import Background from '../../component/studyroom/Background/Background';
 import Timer from '../../component/studyroom/Timer/Timer';
 import TodoList from '../../component/studyroom/Todo/TodoList';
 import SideBar from '../../component/studyroom/SideBar/SideBar';
+import {useRouter} from 'next/router'
 
 interface Test {}
 
 const StudyRoom: Test = () => {
+  const router = useRouter();
+  const roomUrl = router.query;
+  useEffect(()=>{console.log(roomUrl)}, [])
+
   return (
     <>
       <SideBar />
