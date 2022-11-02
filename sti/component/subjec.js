e4f71d8c34aa0508fc914a1aba411579f05ea6c2
soohong <5032303000@naver.com>
2022-11-02 10:52:49 +0900

    feat: subject data add

---
+++ sti/component/subjec.js
@@ -0,0 +1,22 @@
+
+export const subjectArray = [
+  KOREAN,
+  MATH,
+  ENGLISH,
+  KOREANHISTORY,
+  SUB1,
+  SUB2,
+  LANGUAGE,
+  ETC
+];
+
+export const subjectObject = {
+  KOREAN: 국어,
+  MATH: 수학,
+  ENGLISH: 영어,
+  KOREANHISTORY: 한국사,
+  SUB1: 탐구1,
+  SUB2: 탐구2,
+  LANGUAGE: 외국어,
+  ETC: 기타
+};
