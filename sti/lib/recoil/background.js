b18fddc0ddd9620d31bc5bf0356056d1a8eed5d4
soohong <5032303000@naver.com>
2022-11-02 17:23:19 +0900

    feat: background api 주소 설정

--- sti/lib/recoil/background.js
+++ sti/lib/recoil/background.js
@@ -5,6 +5,11 @@ export const backgroundState = atom({
   default: 'https://www.youtube.com/embed/YDodPhpFF9A'
 });
 
+export const backgroundBEState = atom({
+  key: "backgroundBE",
+  default: {bg_id : 0, bg_category: 'CITY', bg_title:'background_title', thumbnail_url:'', bg_url:''}
+});
+
 export const volumeState = atom({
   key: "volume",
   default: 0
