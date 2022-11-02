3a894895a77cc18986328e417a1cdb4a86b0ceb4
김상협 <rotter9027@gmail.com>
2022-11-02 17:15:07 +0900

    feature:카테고리 별 랜덤 background GET

--- backend/src/main/java/com/a401/backend/domain/background/api/BackgroundController.java
+++ backend/src/main/java/com/a401/backend/domain/background/api/BackgroundController.java
@@ -10,6 +10,7 @@ import com.a401.backend.domain.background.application.BackgroundService;
 import com.a401.backend.domain.background.domain.Background;
 import com.a401.backend.domain.background.dto.response.BackgroundResponseDto;
 import com.a401.backend.domain.member.domain.Member;
+import com.a401.backend.domain.model.BackgroundCategory;
 import com.a401.backend.domain.model.RoomAction;
 import com.a401.backend.global.config.security.CurrentUser;
 import com.a401.backend.global.config.security.auth.PrincipalDetails;
@@ -30,9 +31,14 @@ public class BackgroundController {
 
     private final BackgroundService bgService;
 
-    @GetMapping("/{bgId}")
-    public BackgroundResponseDto getBackgroundInfo(@PathVariable("bgId") Long bgId) {
-        return bgService.getBackground(bgId);
+//    @GetMapping("/{bgId}")
+//    public BackgroundResponseDto getBackgroundInfo(@PathVariable("bgId") Long bgId) {
+//        return bgService.getBackground(bgId);
+//    }
+
+    @GetMapping("/{category}")
+    public BackgroundResponseDto getBackgroundInfo(@PathVariable("category") BackgroundCategory category) {
+        return bgService.getRandomBg(category);
     }
 
 }
--- backend/src/main/java/com/a401/backend/domain/background/application/BackgroundServiceImpl.java
+++ backend/src/main/java/com/a401/backend/domain/background/application/BackgroundServiceImpl.java
@@ -3,6 +3,7 @@ package com.a401.backend.domain.background.application;
 import com.a401.backend.domain.background.dao.BackgroundRepository;
 import com.a401.backend.domain.background.domain.Background;
 import com.a401.backend.domain.background.dto.response.BackgroundResponseDto;
+import com.a401.backend.domain.model.BackgroundCategory;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,14 @@ public class BackgroundServiceImpl implements BackgroundService{
         ModelMapper modelMapper = new ModelMapper();
         BackgroundResponseDto bgDto = modelMapper.map(bg,BackgroundResponseDto.class);
 
+        return bgDto;
+    }
+
+    @Override
+    public BackgroundResponseDto getRandomBg(BackgroundCategory category) {
+        Background bg = backgroundRepository.findBackgroundByCategory(category.toString());
+        ModelMapper modelMapper = new ModelMapper();
+        BackgroundResponseDto bgDto = modelMapper.map(bg,BackgroundResponseDto.class);
         return bgDto;
     }
 }
