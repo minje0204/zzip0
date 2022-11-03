9aabc42330c5c21ea4a52e8fd34783bdbe92e198
김상협 <rotter9027@gmail.com>
2022-11-03 15:31:23 +0900

    feature:background 좋아요 리스트 출력

--- backend/src/main/java/com/a401/backend/domain/background/application/BackgroundService.java
+++ backend/src/main/java/com/a401/backend/domain/background/application/BackgroundService.java
@@ -5,8 +5,11 @@ import com.a401.backend.domain.background.dto.response.BackgroundResponseDto;
 import com.a401.backend.domain.member.domain.Member;
 import com.a401.backend.domain.model.BackgroundCategory;
 
+import java.util.List;
+
 public interface BackgroundService {
     BackgroundResponseDto getBackground(Long bgId);
     BackgroundResponseDto getRandomBg(BackgroundCategory category);
     void saveBackgroundLike(BackgroundLikeRegistRequestDto background, Member member);
+    List<BackgroundResponseDto> callBackgroundLike(Member member);
 }
--- backend/src/main/java/com/a401/backend/domain/background/dao/BackgroundRepository.java
+++ backend/src/main/java/com/a401/backend/domain/background/dao/BackgroundRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BackgroundRepository extends JpaRepository<Background,Long> {
     Background findBackgroundByBgId(Long bgId);
 
     @Query(value = "SELECT * FROM background WHERE bg_category = :category ORDER BY RAND() limit 1",nativeQuery = true)
     Background findBackgroundByCategory(@Param("category") String category);
+
+    @Query(value = "SELECT * FROM background LEFT JOIN background_like ON background_like.bg_id = background.bg_id WHERE background_like.member_id = :memberId",nativeQuery = true)
+    List<Background> findbyMember(@Param("memberId") Long memberId);
 }
