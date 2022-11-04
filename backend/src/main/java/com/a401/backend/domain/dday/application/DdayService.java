05a970727996a56a8e47615192fdbd3158c08ac5
김상협 <rotter9027@gmail.com>
2022-11-04 15:23:30 +0900

    feat:Dday 삭제 API 구현

--- backend/src/main/java/com/a401/backend/domain/dday/application/DdayService.java
+++ backend/src/main/java/com/a401/backend/domain/dday/application/DdayService.java
@@ -1,7 +1,6 @@
 package com.a401.backend.domain.dday.application;
 
-import com.a401.backend.domain.dday.dto.request.DdayPostRequestDto;
-import com.a401.backend.domain.dday.dto.request.DdayPutRequestDto;
+import com.a401.backend.domain.dday.dto.request.DdayRequestDto;
 import com.a401.backend.domain.dday.dto.response.DdayResponseDto;
 import com.a401.backend.domain.member.domain.Member;
 
@@ -9,6 +8,7 @@ import java.util.List;
 
 public interface DdayService {
     List<DdayResponseDto> callDday(Member member);
-    void saveDday(DdayPostRequestDto request, Member member);
-    void modifyDday(DdayPutRequestDto request, Member member);
+    void saveDday(DdayRequestDto request, Member member);
+    void modifyDday(DdayRequestDto request, Member member);
+    boolean removeDday(DdayRequestDto request, Member member);
 }
