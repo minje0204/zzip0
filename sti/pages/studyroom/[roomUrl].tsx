864e152eb6f75bf13681df753fbd1e1db50d9185
정지은 <lisajieun@naver.com>
2022-11-04 16:05:09 +0900

    feat: 디데이 컴포넌트 추가 및 위치 수정

--- sti/pages/studyroom/[roomUrl].tsx
+++ sti/pages/studyroom/[roomUrl].tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from 'react';
 import Background from '../../component/studyroom/Background/Background';
 import Timer from '../../component/studyroom/Timer/Timer';
 import TodoList from '../../component/studyroom/Todo/TodoList';
+import Dday from '../../component/studyroom/Dday/Dday';
 import SideBar from '../../component/studyroom/SideBar/SideBar';
 import { useRouter } from 'next/router';
 import makeSocketConnection from '../../component/socket/SocketClient';
@@ -13,6 +14,7 @@ interface Test {}
 const StudyRoom: Test = () => {
   const router = useRouter();
   const roomUrl = router.query;
+
   useEffect(() => {
     console.log(roomUrl['roomUrl']);
     console.log('ddddd');
@@ -25,6 +27,7 @@ const StudyRoom: Test = () => {
       <SideBar />
       <TodoList />
       <Timer />
+      <Dday />
       <Background />
     </>
   );
