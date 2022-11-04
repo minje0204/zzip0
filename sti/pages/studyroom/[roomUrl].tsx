5405992ee708518a56bcd22606d98c1146f09d20
2riing <5032303000@naver.com>
2022-11-04 13:59:26 +0900

    feat: getusername

--- sti/pages/studyroom/[roomUrl].tsx
+++ sti/pages/studyroom/[roomUrl].tsx
@@ -7,19 +7,35 @@ import TodoList from '../../component/studyroom/Todo/TodoList';
 import SideBar from '../../component/studyroom/SideBar/SideBar';
 import { useRouter } from 'next/router';
 import makeSocketConnection from '../../component/socket/SocketClient';
+import { userState } from '../../lib/recoil/member';
+import { useRecoilState } from 'recoil';
+import { getUser } from '../../lib/api/member';
 
 interface Test {}
 
 const StudyRoom: Test = () => {
   const router = useRouter();
   const roomUrl = router.query;
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+
+  const getUserInfo = () => {
+    getUser().then((res) => {
+      setUserInfo(res);
+    });
+  };
+
   useEffect(() => {
-    console.log(roomUrl['roomUrl']);
-    console.log('ddddd');
+    getUserInfo();
     const socketClient = makeSocketConnection(roomUrl['roomUrl']);
     socketClient.activate();
   }, []);
 
+  useEffect(() => {
+    if (userInfo.data) {
+      console.log('user 받아오기', userInfo);
+      console.log('username', userInfo.data.membername);
+    }
+  }, [userInfo]);
   return (
     <>
       <SideBar />
