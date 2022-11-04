0199c64e0e9c30739287af4d62e6558f8a37efe1
김상협 <rotter9027@gmail.com>
2022-11-04 16:18:41 +0900

    feat:Dday GET API에 남은 날짜 컬럼 추가

--- backend/src/main/java/com/a401/backend/domain/dday/dto/response/DdayResponseDto.java
+++ backend/src/main/java/com/a401/backend/domain/dday/dto/response/DdayResponseDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Setter
 @Getter
@@ -16,12 +17,14 @@ public class DdayResponseDto {
     private long ddayId;
     private String ddayTitle;
     private LocalDate ddayDate;
+    private long ddayLeft;
 
     @Builder
     public DdayResponseDto(Dday dday) {
         this.ddayId = dday.getDdayId();
         this.ddayTitle = dday.getDdayTitle();
         this.ddayDate = dday.getDdayDate();
+        this.ddayLeft = LocalDate.now().until(dday.getDdayDate(), ChronoUnit.DAYS);
     }
 
 }
