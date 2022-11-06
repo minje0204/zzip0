b37ae43b1fe7167199d790d9b68eb36bf8cd8842
2riing <5032303000@naver.com>
2022-11-07 01:27:29 +0900

    feat: Create noise get api

---
+++ sti/lib/api/noise.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { Cookies } from 'react-cookie';
+
+const cookies = new Cookies();
+const constantUrl = 'noise';
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_KEY,
+  headers: {
+    Authorization: `Bearer ${cookies.get('accessToken')}`
+  }
+});
+
+export async function getUser() {
+  try {
+    const res = await api.get(`${constantUrl}`);
+    return res;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
