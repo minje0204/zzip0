9bd6dc0de0578e621186d6b857498d78fff91415
soohong <5032303000@naver.com>
2022-11-07 15:34:02 +0900

    chore: add data

--- sti/component/report/MyProfile.tsx
+++ sti/component/report/MyProfile.tsx
@@ -2,13 +2,15 @@
 import {useEffect} from 'react';
 //mui, css
 import home from '../../styles/Home.module.css';
-import { getUser } from '../../lib/api/member'
+import { getUser, updateUser, widthdrawUser } from '../../lib/api/member'
 import { userState } from '../../lib/recoil/member'
 import { useRecoilState } from 'recoil';
 interface Test { }
 
 const MyProfile: Test = () => {
   const [currentUser, setCurrentUser] = useRecoilState(userState)
+  const [data, setData] = userState({membername :'', email :'', profileImage :''})
+  const []
   useEffect(() => {
     getUser().then((res)=>{
       console.log(res)
@@ -21,6 +23,7 @@ const MyProfile: Test = () => {
       <div>{currentUser.email}</div>
       <div>{currentUser.providerId}</div>
       <div>{currentUser.membername}</div>
+      <button>회원정보 수정</button>
     </div>
   );
 };
