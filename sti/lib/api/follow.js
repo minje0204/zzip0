49fcbb1ce709fa10c99fe24fdb58d6d3367c4e35
soohong <5032303000@naver.com>
2022-11-07 17:10:20 +0900

    feat: follow api

--- sti/lib/api/follow.js
+++ sti/lib/api/follow.js
@@ -2,18 +2,37 @@ import axios from 'axios';
 import { Cookies } from 'react-cookie';
 
 const cookies = new Cookies();
-const constantUrl = 'user';
+const constantUrl = 'follow';
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_KEY,
   headers: {
-    // 'Content-Type': 'application/json',
     Authorization: `Bearer ${cookies.get('accessToken')}`
   }
 });
 
-export async function getUser() {
+export async function getFollow() {
   try {
     const res = await api.get(`${constantUrl}/me`);
+    return res;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+export async function postFollow() {
+  try {
+    const res = await api.post(`${constantUrl}/following`);
+    return res;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+export async function deleteFollow() {
+  try {
+    const res = await api.delete(`${constantUrl}/unfollow`);
     return res;
   } catch (err) {
     console.log(err);
