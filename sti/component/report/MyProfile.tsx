ba7f34adc7d57f49c0653079d275b629f0829689
soohong <5032303000@naver.com>
2022-11-07 15:12:47 +0900

    feat: 회원정보 수정, 탈퇴 api, userinfo 받아오기

--- sti/component/report/MyProfile.tsx
+++ sti/component/report/MyProfile.tsx
@@ -1,15 +1,26 @@
 // @ts-nocheck
-import React from 'react';
+import {useEffect} from 'react';
 //mui, css
 import home from '../../styles/Home.module.css';
-
+import { getUser } from '../../lib/api/member'
+import { userState } from '../../lib/recoil/member'
+import { useRecoilState } from 'recoil';
 interface Test { }
 
 const MyProfile: Test = () => {
-
+  const [currentUser, setCurrentUser] = useRecoilState(userState)
+  useEffect(() => {
+    getUser().then((res)=>{
+      console.log(res)
+      setCurrentUser(res.data)
+    })
+  },[])
   return (
     <div className={home.homecontainer}>
       MyProfile
+      <div>{currentUser.email}</div>
+      <div>{currentUser.providerId}</div>
+      <div>{currentUser.membername}</div>
     </div>
   );
 };
