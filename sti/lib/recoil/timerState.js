2779e4a3f76ab84e1d3f29d8dc86b9773d74bb51
정지은 <lisajieun@naver.com>
2022-11-08 15:54:02 +0900

    chore: recoil/timerState에서 필요없는 atom 삭제

--- sti/lib/recoil/timerState.js
+++ sti/lib/recoil/timerState.js
@@ -1,36 +1,17 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
-import { v1 } from 'uuid';
+// import { v1 } from 'uuid';
 
 const { persistAtom } = recoilPersist();
 
-export const subjectTimes = atom({
-  key: `subjectTimes/${v1()}`,
-  default: {
-    korean: 80,
-    math: 100,
-    english: 70,
-    koreanhistory: 30,
-    sub1: 30,
-    sub2: 30,
-    language: 30
-  },
-  effects_UNSTABLE: [persistAtom]
-});
-
-export const selectedSubject = atom({
-  key: `selectedSubject/${v1()}`,
-  default: 'korean'
-});
-
 export const choosedSubjects = atom({
-  key: `choosedSubjects/${v1()}`,
+  key: `choosedSubjects`,
   default: [],
   effects_UNSTABLE: [persistAtom]
 });
 
 export const savedState = atom({
-  key: `savedState/${v1()}`,
-  default: 0,
+  key: `savedState`,
+  default: [],
   effects_UNSTABLE: [persistAtom]
 });
