fb3fe49afe7c3a6edeb2718a4bb4abcf4e0cbae5
김상협 <rotter9027@gmail.com>
2022-11-09 17:52:44 +0900

    feat:cron표현식을 이용한 스케줄러 처리

---
+++ backend/src/main/java/com/a401/backend/domain/timeview/dao/TimeviewDailyRepository.java
@@ -0,0 +1,19 @@
+package com.a401.backend.domain.timeview.dao;
+
+import com.a401.backend.domain.model.Subject;
+import com.a401.backend.domain.timeview.domain.TimeviewDaily;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Repository
+public interface TimeviewDailyRepository extends JpaRepository<TimeviewDaily,Long> {
+    @Query(value = "SELECT count(*) FROM timeview_daily WHERE member_id = :member_id AND date = :date AND subject = :subject",nativeQuery = true)
+    long countByData(long member_id, LocalDate date, Subject subject);
+
+    @Query(value = "SELECT * FROM timeview_daily WHERE member_id = :member_id AND date = :date AND subject = :subject",nativeQuery = true)
+    Optional<TimeviewDaily> findByData(long member_id, LocalDate date, Subject subject);
+}
