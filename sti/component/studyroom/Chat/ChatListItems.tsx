ca5b0bedaec6c7ac1d549f6606afcebf5b3ee3cd
soohong <5032303000@naver.com>
2022-11-10 10:26:18 +0900

    test: socket chat test용 컴포넌트

---
+++ sti/component/studyroom/Chat/ChatListItems.tsx
@@ -0,0 +1,15 @@
+// @ts-nocheck
+
+import React from 'react';
+
+interface Test {}
+
+const ChatListItems: Test = ({ data }) => {
+  return (
+    <div>
+      <span>{data}</span>
+    </div>
+  );
+};
+
+export default ChatListItems;
