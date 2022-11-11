3dc48f5df0efcef2167b67c0380624b38dd5a31e
정지은 <lisajieun@naver.com>
2022-11-11 14:35:52 +0900

    feat: TimerTodo 목표 선택, 레이아웃, 초기값 및 footer 설정

--- sti/component/studyroom/Timer/TimerTodo/TimerTodoFooter.tsx
+++ sti/component/studyroom/Timer/TimerTodo/TimerTodoFooter.tsx
@@ -4,47 +4,108 @@ import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import RadioGroup from '@mui/joy/RadioGroup';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 // recoil
 import { useRecoilState } from 'recoil';
 import { todoTimerState } from '../../../../lib/recoil/todoTimerState';
-import { todoGetAPI } from '../../../../lib/api/todo';
-import { todosState } from '../../../../lib/recoil/todo';
+
 // component
 interface Test {}
 
 const TimerTodoFooter: Test = () => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = ('0' + (today.getMonth() + 1)).slice(-2);
-  const day = ('0' + today.getDate()).slice(-2);
-  const dateStr = year + '-' + month + '-' + day;
-
   const [todoList, setTodoList] = useRecoilState(todoTimerState);
 
-  const getTodayTodos = () => {
-    todoGetAPI(dateStr.replace(/-/g, '')).then((res) => {
-      console.log(res);
-      if (res.data !== '') {
-        setTodoList(res.data);
-      } else {
-        setTodoList([]);
-      }
-    });
-  };
-
-  useEffect(() => {
-    getTodayTodos();
-  }, []);
-
   return (
-
-      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        {todoList.map((todo) => (
-          <div>{todo.subject} : {todo.content} : 시간</div>
-        ))}
-      </List> 
-
+    <>
+      {todoList.length !== 0 ? (
+        <div>
+          <TableContainer>
+            <Table sx={{ width: '250px' }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center" sx={{ width: '10%' }}>
+                    과목
+                  </TableCell>
+                  <TableCell align="center" sx={{ width: '60%' }}>
+                    목표
+                  </TableCell>
+                  <TableCell align="center" sx={{ width: '30%' }}>
+                    걸린 시간
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {todoList.map((todo, idx) => (
+                  <TableRow
+                    key={idx}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      align="center"
+                      sx={{ fontSize: '10px' }}
+                    >
+                      {todo.subject}
+                    </TableCell>
+                    <TableCell align="center" sx={{ fontSize: '10px' }}>
+                      {todo.content}
+                    </TableCell>
+                    <TableCell align="center" sx={{ fontSize: '10px' }}>
+                      00:00:00
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      ) : null}
+    </>
   );
 };
 
 export default TimerTodoFooter;
+
+// const ComputeTime: Test = ({ subjectTime, sub }) => {
+//   const [hour, setHour] = useState(0);
+//   const [min, setMin] = useState(0);
+//   const [sec, setSec] = useState(0);
+//   const [isCountDown, setIsCountDown] = useState(true);
+//   useEffect(() => {
+//     if (subjectTime >= 0) {
+//       setIsCountDown(true);
+//       setHour(Math.floor(subjectTime / 3600));
+//       setMin(Math.floor(subjectTime / 60) % 60);
+//       setSec(subjectTime % 60);
+//     } else {
+//       setIsCountDown(false);
+//       setHour(Math.floor(subjectTime / 3600) * -1 - 1);
+//       setMin((Math.floor(subjectTime / 60) % 60) * -1 - 1);
+//       setSec((subjectTime % 60) * -1);
+//     }
+//   }, [subjectTime]);
+
+//   const originHr = Math.floor(subjectMinutes[sub] / 60);
+//   const originMin = subjectMinutes[sub] % 60;
+//   return (
+//     <>
+//       {subjectTime === undefined ? (
+//         <>
+//           {originHr}:{originMin}:00
+//         </>
+//       ) : (
+//         <>
+//           {isCountDown === false ? <span>- </span> : null}
+//           {hour < 10 ? `0${hour}` : hour}:{min < 10 ? `0${min}` : min}:
+//           {sec < 10 ? `0${sec}` : sec}
+//         </>
+//       )}
+//     </>
+//   );
+// };
