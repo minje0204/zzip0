4585f1ba3ba1158bef3abf5fa38c39d90e948641
김상협 <rotter9027@gmail.com>
2022-11-11 14:13:17 +0900

    feat:일반 타이머, 수능 타이머에 대응하도록 API 수정

--- backend/src/main/java/com/a401/backend/domain/timelog/domain/Timelog.java
+++ backend/src/main/java/com/a401/backend/domain/timelog/domain/Timelog.java
@@ -2,6 +2,7 @@ package com.a401.backend.domain.timelog.domain;
 
 import com.a401.backend.domain.TodoItem.domain.TodoItem;
 import com.a401.backend.domain.member.domain.Member;
+import com.a401.backend.domain.model.Subject;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,6 +25,8 @@ public class Timelog {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "todoitem_id")
     private TodoItem todoitem;
+    @Enumerated(EnumType.STRING)
+    private Subject subject;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
@@ -36,9 +39,11 @@ public class Timelog {
     private LocalTime endTime;
 
     @Builder
-    public Timelog(long timelogId, TodoItem todoitem, Member member, LocalDate date, LocalTime startTime, LocalTime endTime) {
+    public Timelog(long timelogId, TodoItem todoitem, Subject subject, Member member, LocalDate date,
+                   LocalTime startTime, LocalTime endTime) {
         this.timelogId = timelogId;
         this.todoitem = todoitem;
+        this.subject = subject;
         this.member = member;
         this.date = date;
         this.startTime = startTime;
