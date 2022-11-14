c660993c0bdaab1f872e882c7e688148aec4d013
sally <sally2596@gmail.com>
2022-11-14 12:40:45 +0900

    refactor: 타임로그 jpa 더티체킹으로 변경

--- backend/src/main/java/com/a401/backend/domain/timelog/application/TimelogService.java
+++ backend/src/main/java/com/a401/backend/domain/timelog/application/TimelogService.java
@@ -1,10 +1,18 @@
 package com.a401.backend.domain.timelog.application;
 
 import com.a401.backend.domain.member.domain.Member;
+import com.a401.backend.domain.timelog.domain.Timelog;
 import com.a401.backend.domain.timelog.dto.request.TimelogRequestDto;
 import com.a401.backend.domain.timelog.dto.response.TimelogResponseDto;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 public interface TimelogService {
     TimelogResponseDto start(TimelogRequestDto request, Member member);
+
+    @Transactional
     void finish(TimelogRequestDto request, Member member);
+
+    List<Timelog> getTimeLogList(Member member);
 }
