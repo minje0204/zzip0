331500406e3e7554344530a4156ccb2101ad335f
김상협 <rotter9027@gmail.com>
2022-11-14 17:36:30 +0900

    fix:Timeview 호출 API 수정

--- backend/src/main/java/com/a401/backend/domain/timeview/dao/TimeviewDailyRepository.java
+++ backend/src/main/java/com/a401/backend/domain/timeview/dao/TimeviewDailyRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TimeviewDailyRepository extends JpaRepository<TimeviewDaily,Long> {
+    @Query(value = "SELECT * FROM timeview_daily WHERE member_id = :member_id AND date = :date",nativeQuery = true)
+    Optional<TimeviewDaily> findByMemberIdAndDate(long member_id, String date);
     @Query(value = "SELECT * FROM timeview_daily WHERE member_id = :member_id AND date like :date%",nativeQuery = true)
-    Optional<List<TimeviewDaily>> findByMemberIdAndDate(long member_id, String date);
+    Optional<List<TimeviewDaily>> findListByMemberIdAndDate(long member_id, String date);
 }
