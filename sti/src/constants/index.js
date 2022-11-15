3bf36441f9a51dd66beec72636719e41751bc4a1
minje <minje@ajou.ac.kr>
2022-11-15 17:53:47 +0900

    feat: FE코드 원복

--- sti/src/constants/index.js
+++ sti/src/constants/index.js
@@ -1,6 +1,6 @@
-export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'https://zzip0.com';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const OAUTH2_REDIRECT_URI = `${API_BASE_URL}/oauth/redirect`;
 
-export const GOOGLE_AUTH_URL = `${API_BASE_URL}/api/oauth2/authorize/google?redirect_uri=http://localhost:3000`;
+export const GOOGLE_AUTH_URL = `${API_BASE_URL}/api/oauth2/authorize/google?redirect_uri=${API_BASE_URL}`;
