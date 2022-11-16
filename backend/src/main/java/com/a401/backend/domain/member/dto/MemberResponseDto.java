e0c719e0c2f68fb67d8457a7803e63b8f73c0ef4
minje <minje@ajou.ac.kr>
2022-11-16 12:44:09 +0900

    fix: rename mebername

--- backend/src/main/java/com/a401/backend/domain/member/dto/MemberResponseDto.java
+++ backend/src/main/java/com/a401/backend/domain/member/dto/MemberResponseDto.java
@@ -12,25 +12,25 @@ import java.sql.Timestamp;
 @Getter
 @NoArgsConstructor
 public class MemberResponseDto {
-    private String membername;
+    private String memberName;
     private String roles;
     private String provider;
     private String providerId;
     private boolean isActive;
     private String calendarEmail;
-    private String profileImage;
+    private String introduce;
     private String email;
     private Timestamp createDate;
 
     @Builder
     public MemberResponseDto(Member member) {
-        this.membername = member.getMembername();
+        this.memberName = member.getMemberName();
         this.roles = member.getRoles();
         this.provider = member.getProvider();
         this.providerId = member.getProviderId();
         this.isActive = member.isActive();
         this.calendarEmail = member.getCalendarEmail();
-        this.profileImage = member.getProfileImage();
+        this.introduce = member.getIntroduce();
         this.email = member.getEmail();
         this.createDate = member.getCreateDate();
     }
