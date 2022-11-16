4664105a2b52d6e0f9506d44713e592d5b6ded53
soohong <5032303000@naver.com>
2022-11-16 14:26:37 +0900

    feat: Online popover

--- sti/component/studyroom/SideBar/OnlineView.tsx
+++ sti/component/studyroom/SideBar/OnlineView.tsx
@@ -1,9 +1,12 @@
 // @ts-nocheck
-import React, { useEffect } from 'react';
-// mui
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+
+// mui
+import Popover from '@mui/material/Popover';
 // component
 import OnlineItem from './OnlineItem';
+import OnlineListView from './OnlineListView';
 import { useRecoilState } from 'recoil';
 import { myRoomPeopleState, myroomState } from '../../../lib/recoil/room';
 
@@ -12,7 +15,18 @@ interface Test {}
 const OnlineView: Test = () => {
   const [onlines, setOnlines] = useRecoilState(myRoomPeopleState);
   const [roomInfo, setRoomInfo] = useRecoilState(myroomState);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
   useEffect(() => {
     console.log(onlines);
     console.log('onlineview roominfo', roomInfo.memberList);
@@ -21,9 +35,7 @@ const OnlineView: Test = () => {
     }
   }, [roomInfo]);
 
-  useEffect(() => {
-    console.log(onlines);
-  }, [onlines]);
+  useEffect(() => {}, [onlines]);
 
   return (
     <OnlineViewContainer>
@@ -31,14 +43,30 @@ const OnlineView: Test = () => {
         참가자 목록
         <br />
         {Array.isArray(onlines) && onlines.length > 0 ? (
-          <>
-            {console.log('ddddddddddd', onlines)}
+          <div onClick={handleClick}>
             {onlines.map((data) => (
               <OnlineItem key={data} data={data} />
             ))}
-          </>
+          </div>
         ) : null}
       </div>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'right'
+        }}
+        transformOrigin={{
+          vertical: 'center',
+          horizontal: 'left'
+        }}
+      >
+        <OnlineListView datas={onlines} />
+      </Popover>
+
       <div></div>
     </OnlineViewContainer>
   );
