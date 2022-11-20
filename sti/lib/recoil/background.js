102e103abdfb5cb4284b86e57326205fb695b54d
sally2596@gmail.com <sally2596@gmail.com>
2022-11-20 19:01:58 +0900

    chore: 초기 백그라운드 값 없앰

--- sti/lib/recoil/background.js
+++ sti/lib/recoil/background.js
@@ -4,12 +4,12 @@ import { v1 } from 'uuid';
 export const backgroundBEState = atom({
   key: `backgroundBEApi/${v1()}`,
   default: {
-    bgId: 1,
-    bgCategory: 'CHRISTMAS',
-    bgTitle: 'background_title',
-    thumbnailUrl: '',
-    bgUrl: 'https://www.youtube.com/embed/Ihw_I-zvu1Q',
-    bgmUrl: 'https://a401.s3.ap-northeast-2.amazonaws.com/bgm/CHRISTMAS01.mp3'
+    // bgId: 1,
+    // bgCategory: 'CHRISTMAS',
+    // bgTitle: 'background_title',
+    // thumbnailUrl: '',
+    // bgUrl: 'https://www.youtube.com/embed/Ihw_I-zvu1Q',
+    // bgmUrl: 'https://a401.s3.ap-northeast-2.amazonaws.com/bgm/CHRISTMAS01.mp3'
   }
 });
 
